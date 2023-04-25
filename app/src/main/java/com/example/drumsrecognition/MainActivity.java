50b5b414199e442958c3c8d21851ad252435e029
PedroRaphaelGomes <pedro.gomes@estudante.ifgoiano.edu.br>
2023-04-25 16:33:03 -0300

    adicionando dados aceleremetro

--- app/src/main/java/com/example/drumsrecognition/MainActivity.java
+++ app/src/main/java/com/example/drumsrecognition/MainActivity.java
@@ -28,11 +28,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     List<List<String>> lista1 = new ArrayList<List<String>>();
     List<List<String>> lista2 = new ArrayList<List<String>>();
 
-    MDDTW mddtw;
-
     Boolean Treino = false, Compara = false;
     long timeStart = 0;
-    Boolean dtw = false;
+    //Boolean dtw = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,25 +40,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        dado = findViewById(R.id.txtDist);
+
         btncompara = findViewById(R.id.btnCompara);
         btntreino = findViewById(R.id.btnTreino);
 
         btntreino.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("OLA", "onSensorChanged: Clicou Treino");
-                Treino = true;
+                Log.d("OLA", "Clicou Treino");
+                //Treino = true;
                 timeStart = System.currentTimeMillis();
 
-                if(Treino){
-                    List<String> dado = new ArrayList<String>();
-                    dado.add("1.7");
-                    dado.add("2.3");
-                    dado.add("4.5");
-                    lista2.add(dado);
-                    lista2.add(dado);
+                if(!Treino){
 
-                    Log.d("OLA", "onSensorChanged: Treino Start");
+                    start();
+                    Log.d("OLA", "Treino Start");
+                    Treino = true;
+
+                }else{
+                    stop();
+                    //lista1.add(dados);
+                    //dados.clear();
+
                     Treino = false;
                 }
 
@@ -70,32 +72,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         btncompara.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("OLA", "onSensorChanged: Clicou Compara");
-                Compara = true;
+                Log.d("OLA", "Clicou Compara");
+                //Compara = true;
                 timeStart = System.currentTimeMillis();
 
-                if(Compara){
-                    List<String> dado = new ArrayList<String>();
-                    dado.add("1.6");
-                    dado.add("2.2");
-                    dado.add("4.6");
-                    lista1.add(dado);
-                    lista1.add(dado);
+                if(!Compara){
 
+                    start();
+                    Log.d("OLA", "Compara Start");
+                    Compara = true;
 
-                    Log.d("OLA", "onSensorChanged: Compara Start");
-                    Compara = false;
-                    dtw = true;
+                }else{
+                    stop();
+                    MDDTW mddtw;
 
-                    if(dtw){
-                        SensorData sensorData1 = new SensorData(lista1);
-                        SensorData sensorData2 = new SensorData(lista2);
+                    SensorData sensorData1 = new SensorData(lista1);
+                    SensorData sensorData2 = new SensorData(lista2);
 
-                        Log.d("OLA", "onSensorChanged: X, Y e Z : " + "X1: " + sensorData1.getXData() + "X2: " + sensorData2.getXData());
-                        mddtw = new MDDTW(sensorData1,sensorData2);
-                        Log.d("OLA", "onSensorChanged: Compara Start" + mddtw.getDistancia());
-                        dtw = false;
-                    }
+                    Log.d("OLA", "X, Y e Z : " + "X1: " + sensorData1.getXData() + "X2: " + sensorData2.getXData());
+                    mddtw = new MDDTW(sensorData1,sensorData2);
+                    Log.d("OLA", "Distancia: " + mddtw.getDistancia());
+                    dado.setText("" + mddtw.getDistancia());
+
+                    lista1.clear();
+                    lista2.clear();
                 }
             }
         });
@@ -110,34 +110,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
-        String gravity0 = String.valueOf(sensorEvent.values[0]);
-        String gravity1 = String.valueOf(sensorEvent.values[1]);
-        String gravity2 = String.valueOf(sensorEvent.values[2]);
-        //lista.add(dados);
-        //dados.clear();
+        String gravity0 = Float.toString(sensorEvent.values[0]);
+        String gravity1 = Float.toString(sensorEvent.values[1]);
+        String gravity2 = Float.toString(sensorEvent.values[2]);
+
+        dados.add(gravity0);
+        dados.add(gravity1);
+        dados.add(gravity2);
 
         if(Treino){
-            Log.d("OLA", "onSensorChanged: Treino Start");
-            if(System.currentTimeMillis() - timeStart > 1200){
-                Log.d("OLA", "onSensorChanged: Treino FIM");
-                Treino = false;
-            }
-            dados.add(gravity0);
-            dados.add(gravity1);
-            dados.add(gravity2);
+            Log.d("OLA", "Treino Adicinando");
+            lista1.add(new ArrayList<String>(dados));
+            dados.clear();
         }
 
         if(Compara){
-            Log.d("OLA", "onSensorChanged: Compara Start");
-            if(System.currentTimeMillis() - timeStart > 1200){
-                Log.d("OLA", "onSensorChanged: Compara FIM");
-                Compara = false;
-                dtw = true;
-            }
-
+            Log.d("OLA", "Compara Adicinando");
+            lista2.add(new ArrayList<String>(dados));
+            dados.clear();
         }
 
-        //Log.d("OLA", "onSensorChanged: X, Y e Z : " + "X: " + gravity0 + "Y: " + gravity1 + "Z : " + gravity2);
+        //Log.d("OLA", "onSensorChanged: Treino Start" + " "  + gravity0 + " "  + gravity2 + " "  + gravity1);
+
     }
 
     @Override
@@ -153,20 +147,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.unregisterListener(this);
     }
 
-    public MainActivity() {
-        super();
-    }
 
-    @Override
-    protected void onPause() {
-        stop();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        start();
-        super.onResume();
-    }
 
 }
